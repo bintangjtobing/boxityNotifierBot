7bfedfa5ae4d90333c9e79ea01b3774f8dab6805
bintangjtobing <hello@bintangtobing.com>
2021-08-23 10:21:33 +0700

    feat first telegram

---
+++ bot.rb
@@ -0,0 +1,23 @@
+require 'telegram_bot'
+token = '1968855714:AAEOhVqRUTgLJ4JdQ2ZBoSao43dQTVHF1fE'
+bot = TelegramBot.new(token: token)
+
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+
+  message.reply do |reply|
+    case command
+    when /hello/i
+      reply.text = "Hello, #{message.from.first_name}. Hope you fine today ☀️."
+    when /start/i
+      reply.text = "All I can do is say hello. Try the /hello command."
+    when /getid/i
+      reply.text = "This is your telegram id: #{message.from.id}"
+    else
+      reply.text = "I have no idea what #{command.inspect} means."
+    end
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
+  end
+end
